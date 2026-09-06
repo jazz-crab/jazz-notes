076a012f7487b91ca2050d16104b8ff508dff73f
jazz-crab <tvesnz@gmail.com>
2026-09-07 02:40:00 +0500

    web: активная заметка выступает на 5px влево (не проценты)

--- src/renderer/src/screens/NoteList.tsx
+++ src/renderer/src/screens/NoteList.tsx
@@ -74,7 +74,7 @@ function NoteItem({ note, isDeleting, isActive, isLastOpened, onOpen, onHover, o
         transform: CSS.Transform.toString(transform),
         opacity: isDragging ? 0.5 : 1,
         touchAction: 'none',
-        width: isActive ? '100%' : '98%',
+        width: isActive ? '100%' : 'calc(100% - 5px)',
         marginLeft: isActive ? 0 : 'auto',
       }}
     >
